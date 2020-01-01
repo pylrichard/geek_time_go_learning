601c1e5681885b27306dda53f1018907d6f32631
pylrichard <pylrichard@qq.com>
2020-01-01 11:18:10 +0800

    ch1

---
+++ ch1/hello/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+/*
+	go build . & ./hello pyl
+	退出码通过echo $?查看
+ */
+func main() {
+	if len(os.Args) > 1 {
+		fmt.Println("Hello", os.Args[1])
+	}
+	os.Exit(-1)
+}
