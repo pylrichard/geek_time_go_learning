d7bd9b22ce693ce3ea0b9474c8b08c77564e4e14
pylrichard <pylrichard@qq.com>
2020-01-11 12:14:44 +0800

    ch12

--- ch11/encapsulation/encapsulation_test.go
+++ ch11/encapsulation/encapsulation_test.go
@@ -1,4 +1,4 @@
-package encapsulation
+package encapsulation_test
 
 import (
 	"fmt"
---
+++ ch12/extension/extension_test.go
@@ -0,0 +1,32 @@
+package extension_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Pet struct {
+}
+
+func (p *Pet) Speak() {
+	fmt.Printf("...")
+}
+
+func (p *Pet) SpeakTo(host string) {
+	p.Speak()
+	fmt.Println("", host)
+}
+
+type Dog struct {
+	Pet
+}
+
+func (d *Dog) Speak() {
+	fmt.Print("!")
+}
+
+func TestDog(t *testing.T) {
+	dog := new(Dog)
+
+	dog.SpeakTo("Chao")
+}
